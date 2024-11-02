f84dd190913d2bf337d33a0221fd6402c6592e21
nelturion <aandreyuk2001@gmail.com>
2024-11-02 04:49:52 +0300

    [feat]: implemented vigenere (de)encryption functions

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -1,28 +1,119 @@
+import string
+
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
-    >>> encrypt_vigenere("PYTHON", "A")
+    #>>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
-    >>> encrypt_vigenere("python", "a")
+    #>>> encrypt_vigenere("python", "a")
     'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    #>>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
+    #>>> encrypt_vigenere("AttackAtDawn", "lemon")
+    'LxfopvEfRnhr'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    # create alphabet of both upper and lower case letters
+    lower_chars = dict(enumerate([char for char in string.ascii_lowercase]))
+    inverted_lower_chars = {v: k for k, v in lower_chars.items()}
+    upper_chars = dict(enumerate([char for char in string.ascii_uppercase]))
+    inverted_upper_chars = {v: k for k, v in upper_chars.items()}
+
+    key = full_and_even_key(plaintext, keyword)  # дополняем keyword до длины слова
+
+    # получаем индексы букв КЛЮЧА и СЛОВА в алфавите
+    key_letter_nums = [str(inverted_upper_chars[ch]) if ch.isupper() else inverted_lower_chars[ch] for ch in key]
+    word_letter_nums = [str(inverted_upper_chars[ch]) if ch.isupper() else inverted_lower_chars[ch] for ch in plaintext]
+
+    # создаем массив в котором будут храниться алфавитные номера букв согласно алгоритму Виженера
+    enc_letters_list = []
+    for a, b in zip(word_letter_nums, key_letter_nums):  # итерируемся по индексам' из обоих списков КЛЮЧА и СЛОВА
+        enc_letters_list.append(str((int(a) + int(b)) % 26) if type(a) is str else (int(a) + int(b)) % 26)
+
+    # создаем строку из алфавитных номеров полученных символов
+    ciphertext = "".join(
+        upper_chars[int(element)] if type(element) is str else lower_chars[element] for element in enc_letters_list
+    )
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
-    >>> decrypt_vigenere("PYTHON", "A")
+    #>>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
-    >>> decrypt_vigenere("python", "a")
+    #>>> decrypt_vigenere("python", "a")
     'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    #>>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
+    #>>> decrypt_vigenere("LxfopvEfRnhr", "LeMoN")
+    'AttackAtDawn'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    # create alphabet of both upper and lower case letters
+    lower_chars = dict(enumerate([char for char in string.ascii_lowercase]))
+    inverted_lower_chars = {v: k for k, v in lower_chars.items()}
+    upper_chars = dict(enumerate([char for char in string.ascii_uppercase]))
+    inverted_upper_chars = {v: k for k, v in upper_chars.items()}
+
+    keyword = full_and_even_key(ciphertext, keyword)  # дополняем keyword до длины слова
+
+    # получаем индексы букв КЛЮЧА и СЛОВА в алфавите
+    key_letter_nums = [str(inverted_upper_chars[ch]) if ch.isupper() else inverted_lower_chars[ch] for ch in keyword]
+    word_letter_nums = [str(inverted_upper_chars[ch]) if ch.isupper() else inverted_lower_chars[ch] for ch in
+                        ciphertext]
+
+    # создаем массив в котором будут храниться алфавитные номера букв согласно алгоритму Виженера
+    enc_letters_list = []
+    for a, b in zip(word_letter_nums, key_letter_nums):
+        if type(a) is str:
+            a = int(a)
+            b = int(b)
+            x = str(a - b if (a - b) >= 0 else 26 - abs(a - b))
+        else:
+            # гугля, как можно было бы это реализовать по-другому, я пришел к идее использовать
+            # отрицательный индекс для list(lower_chars.values()); реализовывать не стал.
+            x = (a - b if (a - b) >= 0 else 26 - abs(a - b))
+        enc_letters_list.append(x)
+
+    # воссоздаем слово из алфавитных индексов его букв учитывая регистр
+    plaintext = "".join(
+        upper_chars[int(element)] if type(element) is str else lower_chars[element] for element in enc_letters_list
+    )
+
     return plaintext
+
+
+def full_and_even_key(word, key):
+    # fill keys to fit in word len (or make it a bit bigger)
+    keyword = key
+    while len(keyword) < len(word):
+        keyword += key
+
+    # выравниваем списки по длине функцией zip и берем в new_keyword только значения из list(keyword)
+    new_keyword = [x[1] for x in list(zip(list(word), list(keyword)))]
+    for i in range(len(new_keyword)):  # выравниваем регистр букв по регистру plaintext/cipher слова
+        if word[i].isupper():
+            new_keyword[i] = new_keyword[i].upper()
+        if word[i].islower():
+            new_keyword[i] = new_keyword[i].lower()
+
+    return "".join(new_keyword)  # итоговое ключ-слово длина и регистр символов которого приведены к изначальному
+
+
+if __name__ == "__main__":
+    # set up for program
+    mode = input("Select mode: encrypt/decrypt (e/d) ")
+    message = input("Put text here: ")
+    keyword = input("Put keyword (one word) here: ")
+
+    # algorithm
+    words = message.split()
+
+    if mode == "e" or mode == "encrypt":
+        print(" ".join(encrypt_vigenere(word, keyword) for word in words))
+    else:
+        print(" ".join(decrypt_vigenere(word, keyword) for word in words))
---
+++ tests/lab2/test_vigenre.py
@@ -0,0 +1,20 @@
+import unittest
+from src.lab2 import vigenre as v
+
+
+class CalculatorTestCase(unittest.TestCase):
+    def test_encryption(self):
+        self.assertEqual(v.encrypt_vigenere("PYTHON", "A"), 'PYTHON')
+        self.assertEqual(v.encrypt_vigenere("python", "a"), 'python')
+        self.assertEqual(v.encrypt_vigenere("ATTACKATDAWN", "LEMON"), 'LXFOPVEFRNHR')
+        self.assertEqual(v.encrypt_vigenere("AttackAtDawn", "LeMoN"), 'LxfopvEfRnhr')
+
+    def test_decryption(self):
+        self.assertEqual(v.decrypt_vigenere("PYTHON", "A"), 'PYTHON')
+        self.assertEqual(v.decrypt_vigenere("python", "a"), 'python')
+        self.assertEqual(v.decrypt_vigenere("LXFOPVEFRNHR", "LEMON"), 'ATTACKATDAWN')
+        self.assertEqual(v.decrypt_vigenere("LxfopvEfRnhr", "LeMoN"), 'AttackAtDawn')
+
+
+if __name__ == '__main__':
+    unittest.main()
