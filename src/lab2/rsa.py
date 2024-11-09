6ee8f913e6af4ac22837b2fc6d244c87532db740
nelturion <aandreyuk2001@gmail.com>
2024-11-09 21:51:31 +0300

    [feat]: реализована функция is_prime()

--- src/lab2/rsa.py
+++ src/lab2/rsa.py
@@ -9,11 +9,23 @@ def is_prime(n: int) -> bool:
     True
     >>> is_prime(11)
     True
+    >>> is_prime(17)
+    True
     >>> is_prime(8)
     False
+    >>> is_prime(1)
+    False
+    >>> is_prime(0)
+    False
     """
-    # PUT YOUR CODE HERE
-    pass
+    if n == 1:
+        return False
+    if n % 2 == 0:
+        return n == 2
+    d = 3
+    while d * d <= n and n % d != 0:
+        d += 2
+    return d * d > n
 
 
 def gcd(a: int, b: int) -> int:
