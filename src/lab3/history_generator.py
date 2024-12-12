d40f9284d1a1d21d355040aa9d23234f67e91b5e
nelturion <aandreyuk2001@gmail.com>
2024-12-12 16:48:54 +0300

    [feat]: условие задачи + генератор для него

---
+++ src/lab3/history_generator.py
@@ -0,0 +1,18 @@
+import random
+import random as r
+
+
+def generate_history(min_story_len: int = 1, max_story_len: int = 10, user_amount: int = 10):
+    with open("resources/txt/watch_history.txt", "w") as file:
+        for j in range(user_amount):
+            lst = []
+            story_len = random.randint(min_story_len, max_story_len)
+            for i in range(story_len):
+                lst.append(str(r.randint(1, 9)))
+            s = ",".join(lst)
+            file.writelines(s)
+            file.write("\n")
+
+
+if __name__ == "__main__":
+    generate_history(3, 15, 100)
