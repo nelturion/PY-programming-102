d48d7863719bf564368a35a37e1f913651918501
nelturion <aandreyuk2001@gmail.com>
2026-05-19 14:34:29 +0300

    [fix]: fixed code style

--- src/sem2/lab1/decorators.py
+++ src/sem2/lab1/decorators.py
@@ -7,26 +7,27 @@ from functools import wraps  # Импортируем wraps
 
 def logger(func):
     """
-        Задание 1 - декоратор без собственных параметров:
-        Реализовать декоратор logger, который выводит имя функции, её аргументы и время выполнения. Можно добавить вывод результата.
+    Задание 1 - декоратор без собственных параметров:
+    Реализовать декоратор logger, который выводит имя функции, её аргументы и время выполнения. Можно добавить вывод результата.
 
-        >>> @logger
-        ... def add(a, b):
-        ...     return a + b
-        >>> add(2, 3) # doctest: +ELLIPSIS
-        [...] FUNCTION CALL: add
-          ARGUMENTS: args=(2, 3), kwargs={}
-          TIME:      ... sec
-          MEMORY:    ... KiB (Peak: ... KiB)
-        ----------------------------------------
-        <BLANKLINE>
-        5
+    >>> @logger
+    ... def add(a, b):
+    ...     return a + b
+    >>> add(2, 3) # doctest: +ELLIPSIS
+    [...] FUNCTION CALL: add
+      ARGUMENTS: args=(2, 3), kwargs={}
+      TIME:      ... sec
+      MEMORY:    ... KiB (Peak: ... KiB)
+    ----------------------------------------
+    <BLANKLINE>
+    5
     """
+
     @wraps(func)  # Сохраняет имя и docstring оригинальной функции
     def wrapper(*args, **kwargs):
         cmd_silent = "--silent" in sys.argv
         manual_silent = kwargs.pop("silent", False)
-        global_silent = globals().get('silent', False)
+        global_silent = globals().get("silent", False)
         should_be_silent = cmd_silent or manual_silent or global_silent
 
         tracemalloc.start()
@@ -46,7 +47,9 @@ def logger(func):
                 print(f"  TIME:      {duration:.6f} sec")
                 print(f"  MEMORY:    {current / 1024:.2f} KiB (Peak: {peak / 1024:.2f} KiB)")
                 print(f"{'-' * 40}\n")
+
     return wrapper
+
 
 @logger
 def loggable_function(text: str, n: int):
@@ -56,6 +59,7 @@ def loggable_function(text: str, n: int):
     'some text... 6'
     """
     return f"{text}... {n + n * n}"
+
 
 # def uppercase(func):
 #     def wrapper(*args, **kwargs):
